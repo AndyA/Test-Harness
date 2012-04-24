5b9ff459422754c6b67a31674503e1253356913c
Andy Armstrong <andy@hexten.net>
2012-04-24 19:40:45 +0100

    Move to Perl-Toolchain-Gang

--- misc/togit/cvt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-perl svn-reloc.pl < tapx.svn > tapx.svn.cvt || exit $?
-mkdir -p svn
-rm -rf svn/tapx tapx
-svnadmin create svn/tapx
-cat tapx.svn.cvt | svnadmin load svn/tapx || exit $?
-svn co "file://`pwd`/svn/tapx"
--- xt/author/stdin.t
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-
-use strict;
-use lib 't/lib';
-
-use Test::More tests => 1;
-
-ok -t STDIN, 'STDIN remains a TTY';
